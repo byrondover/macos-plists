ac2d7cd02467e37685894834d961959ea4833a08
Byron Dover <byrondover+github@gmail.com>
2019-11-17 05:27:47 -0800

    Update plists

--- gather.sh
+++ gather.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+IFS=$'\n'
+
 for file in $(find . -type f -path '\./Library/[A-Za-z]*' \! -path '\./\.git*')
 do
-  cp -p /$file $file
+  cp -p "/${file}" "${file}"
 done
 
 for file in $(find . -type f -path '\./home/Library/[A-Za-z]*' \! -path '\./\.git*')
 do
-  cp -p ~/$(cut -d '/' -f 3- <<< $file) $file
+  cp -p "${HOME}/$(cut -d '/' -f 3- <<< $file)" "${file}"
 done
